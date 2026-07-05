51f6f74d41a98f7a9966d1ffa79a2d9478c81dcb
IMRAN AHMED <imran.info.me@gmail.com>
2026-07-05 13:37:27 +0600

    Create js/config.js

---
+++ js/config.js
@@ -0,0 +1,12 @@
+// js/config.js
+const CONFIG = {
+    FIREBASE: {
+        apiKey: "AIzaSyDRfX8cqKmDRV2u1HfH2opa5tB2NqD0h9s",
+        authDomain: "techverse-eee-cse.firebaseapp.com",
+        projectId: "techverse-eee-cse",
+        storageBucket: "techverse-eee-cse.firebasestorage.app",
+        messagingSenderId: "719941320241",
+        appId: "1:719941320241:web:10285cc72447a811ad5f10"
+    },
+    WEB3FORMS_KEY: "35301924-2be3-4ce1-965d-b9d61eb068f5"
+};
